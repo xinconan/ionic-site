c8eda59edb77d73d673d64d3cf0726932d3b7981
Max Lynch <ihasmax@gmail.com>
2015-03-30 20:44:42 -0500

    Dirname

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
 var express = require("express");
 var app = express();
 
-process.env.PWD = process.cwd()
-
-app.use(express.static(process.env.PWD + '/_site'));
-
+app.use(express.static(__dirname + '/_site'));
 
 // bind the app to listen for connections on a specified port
 var port = process.env.PORT || 3000;
