57e9e73902a7fad599c4ca695adafbe4463e48f0
Max Lynch <ihasmax@gmail.com>
2015-03-30 20:35:12 -0500

    CWD

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 var express = require("express");
 var app = express();
 
-app.use(express.static(__dirname + '/_site'));
+process.env.PWD = process.cwd()
+
+app.use(express.static(process.env.PWD + '/_site'));
 
 
 // bind the app to listen for connections on a specified port
