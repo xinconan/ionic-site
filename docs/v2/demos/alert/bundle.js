1db0080b17e0b1f42f9ca4794da570032710b4bc
ionitron <hi@ionicframework.com>
2016-02-29 19:38:42 +0000

    automated build of 1a38d69a3269d24d859d0837b40ce48caf408859

--- docs/v2/demos/alert/bundle.js
+++ docs/v2/demos/alert/bundle.js
@@ -50826,11 +50826,12 @@
 	        if (this.state === STATE_INACTIVE) {
 	            // this refresh is not already actively pulling down
 	            // get the content's scrollTop
-	            var scrollHostScrollTop = this._content.scrollElement.scrollTop;
+	            var scrollHostScrollTop = this._content.getContentDimensions().scrollTop;
 	            // if the scrollTop is greater than zero then it's
 	            // not possible to pull the content down yet
 	            if (scrollHostScrollTop > 0) {
 	                this.progress = 0;
+	                this.startY = null;
 	                return 7;
 	            }
 	            // content scrolled all the way to the top, and dragging down
@@ -63309,8 +63310,6 @@
 	            }
 	        });
 	        this.nav.present(alert);
-	    };
-	    InitialPage.prototype.doCustomAnimation = function () {
 	    };
 	    InitialPage = __decorate([
 	        ionic_angular_1.Page({
